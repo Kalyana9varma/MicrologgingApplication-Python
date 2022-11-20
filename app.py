536b426a0f15d08640902d4e6d1be79242ee7319
Kalyana9varma <97426496+Kalyana9varma@users.noreply.github.com>
2022-11-20 16:59:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def create_app():
         if request.method == "POST":
             entry_content = request.form.get("content")
             formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-            app.db.entries.insert({"content": entry_content, "date": formatted_date})
+            app.db.test.insert({"content": entry_content, "date": formatted_date})
         
         entries_with_date = [
             (
@@ -24,7 +24,7 @@ def create_app():
                 entry["date"],
                 datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
             )
-            for entry in app.db.entries.find({})
+            for entry in app.db.test.find({})
         ]
         return render_template("home.html", entries=entries_with_date)
     
